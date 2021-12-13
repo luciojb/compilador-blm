5de1d2f117454dec111115528ceb3890c1e72ea7
Lúcio José Beirão <lucio.beirao@effecti.com.br>
2021-12-13 20:23:04 -0300

    Atualizando lexer e adicionando requirements.txt

--- lexer.py
+++ lexer.py
@@ -1,11 +1,5 @@
 from rply import LexerGenerator
 
-# tokens = {
-#     'PRINT': r'print',
-# 'OPEN_PAREN', r'\(',
-# 'CLOSE_PAREN', r'\)',
-# 'SEMI_COLON', r'\;'
-# }
 
 class Lexer():
     def __init__(self):
@@ -17,13 +11,49 @@ class Lexer():
         # Parenthesis
         self.lexer.add('OPEN_PAREN', r'\(')
         self.lexer.add('CLOSE_PAREN', r'\)')
-        # Semi Colon
-        self.lexer.add('SEMI_COLON', r'\;')
+        #KEYS
+        self.lexer.add('LBRACE', r'\{')
+        self.lexer.add('RBRACE', r'\}')
+        #BRACKETS
+        self.lexer.add('OPEN_BRACKETS', r'\[')
+        self.lexer.add('CLOSE_BRACKETS', r'\]')
+        
         # Operators
         self.lexer.add('SUM', r'\+')
         self.lexer.add('SUB', r'\-')
+        self.lexer.add('DIV', r'\/')
+        self.lexer.add('MULT', r'\*')
         # Number
         self.lexer.add('NUMBER', r'\d+')
+        # Semi Colon
+        self.lexer.add('SEMI_COLON', r'\;')
+        # COMMA
+        self.lexer.add('COMMA', r'\,')
+        # LESS
+        self.lexer.add('LESS', r'\<')
+        # GREATER
+        self.lexer.add('GREATER', r'\>')
+        # EQUAL
+        self.lexer.add('EQUAL', r'\=')
+        # EQEQUAL
+        self.lexer.add('EQEQUAL', r'\=\=')
+        # DOT
+        self.lexer.add('DOT', r'\.')
+        # PERCENT
+        self.lexer.add('PERCENT', r'\%')
+        # NOTEEQUAL
+        self.lexer.add('NOTEQUAL', r'\!\=')
+        # LESSEQUAL
+        self.lexer.add('LESSEQUAL', r'\<\=')
+        # GREATEQUAL
+        self.lexer.add('GREATEQUAL', r'\>\=')
+        # IF
+        self.lexer.add('IF', r'\I\F')
+        # VBAR
+        self.lexer.add('VBAR', r'\|')
+        # AMPER
+        self.lexer.add('AMPER', r'\&')
+
         # Ignore spaces
         self.lexer.ignore('\s+')
 
